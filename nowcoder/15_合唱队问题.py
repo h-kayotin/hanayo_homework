f970ba7a77beaa69e3c398ca0abf46bfc2e7f7fb
kayotin <hanayo@kayotin.cn>
2024-03-22 22:23:02 +0800

    update

--- nowcoder/15_合唱队问题.py
+++ nowcoder/15_合唱队问题.py
@@ -8,31 +8,46 @@
 Author: hanayo
 Date： 2024/3/22
 """
+import bisect
 
 
-def find_max_add(nums):
+def find_up_sub(nums):
     """寻找最长递增序列"""
-    for i in range(1, len(nums)):
-        for j in range(i+1, len(nums)):
-            pass
+    # 完全遍历，n^2
+    n = len(nums)
+    up_list = [1] * n
+    for i in range(n):
+        for ii in range(i):
+            if nums[ii] < nums[i]:
+                up_list[i] = max(up_list[i], up_list[ii] + 1)
+    return up_list
 
 
-def main():
-    team_len = int(input())
-    if team_len == 1:
-        return 0
-    heights_str = str(input())
-    h_list = [int(x) for x in heights_str.split(" ")]
-    max_h = max(h_list)
-    max_index = 0
-    for h in h_list:
-        max_index += 1
-        if h == max_h:
-            break
-    print(max_index)
-    left_list = h_list[0: max_index]
-    right_light = h_list[max_index:]
+def up_sub2(nums):
+    # 用二分查找，加快了速度
+    n = len(nums)
+    dp = [1] * n
+    arr = [nums[0]]
+    for i in range(1, n):
+        if nums[i] > arr[-1]:
+            arr.append(nums[i])
+            dp[i] = len(arr)
+        else:
+            # 用二分查找到arr中第一个比nums[i]大的元素的位置
+            pos = bisect.bisect_left(arr, nums[i])
+            arr[pos] = nums[i]
+            dp[i] = pos + 1
+        print(arr)
+    return dp
 
 
-if __name__ == '__main__':
-    main()
+while True:
+    try:
+        N = int(input())
+        s = list(map(int, input().split()))
+        left_s = up_sub2(s) # 从左至右
+        right_s = up_sub2(s[::-1])[::-1] # 从右至左
+        sum_s = [left_s[i]+right_s[i]-1 for i in range(len(s))] # 相加并减去重复计算
+        print(str(N-max(sum_s)))
+    except EOFError:
+        break
