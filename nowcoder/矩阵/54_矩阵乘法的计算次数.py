43737659ca4c8dea15ebcedf8ec8b071abf8ba78
kayotin <hanayo@kayotin.cn>
2024-04-20 11:01:20 +0800

    update

---
+++ nowcoder/矩阵/54_矩阵乘法的计算次数.py
@@ -0,0 +1,21 @@
+"""
+54_矩阵乘法的计算次数 - 
+
+矩阵乘法 a矩阵 x行y列 c矩阵y行z列，实际计算次数是x*y*z
+
+Author: kayotin
+Date 2024/4/20
+"""
+
+
+def count_of_ops(tp_list: list[tuple]):
+    pass
+
+
+n = int(input())
+xys = []
+for _ in range(n):
+    x, y = str(input()).split(" ")
+    xys.append((int(x), int(y)))
+turn_str = str(input())
+
