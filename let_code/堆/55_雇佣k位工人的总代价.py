780a964bbe0a2f8ec97d823bb039cc991df1fd20
JiangHai <hanayo@kayotin.cn>
2024-05-01 19:02:55 +0800

    update

--- let_code/堆/55_雇佣k位工人的总代价.py
+++ let_code/堆/55_雇佣k位工人的总代价.py
@@ -10,16 +10,19 @@ from heapq import heapify
 
 
 def total_cost(self, costs: list[int], k: int, candidates: int) -> int:
+    # 必定能取到前k个最小的元素，直接返回他们的和即可
     n = len(costs)
     if candidates*2 + k > n:
         costs.sort()
         return sum(costs[:k])
 
+    # 初始化堆
     l_heap = costs[: candidates]
     r_heap = costs[-candidates:]
     heapify(l_heap)
     heapify(r_heap)
 
+    # 每次执行比较，左右堆中较小的出堆，然后通过下标补充进堆
     ans = 0
     i = candidates
     j = n - 1 - candidates
