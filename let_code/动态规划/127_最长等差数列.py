fc3a680cf49dd6d17e1b26331eb8b749d2509132
JiangHai <hanayo@kayotin.cn>
2024-07-30 17:29:51 +0800

    update

--- let_code/动态规划/127_最长等差数列.py
+++ let_code/动态规划/127_最长等差数列.py
@@ -9,4 +9,16 @@ from typing import List
 
 
 def longest_arith_seq_length(self, nums: List[int]) -> int:
-    pass
+    """枚举公差"""
+    n = len(nums)
+    f = [[1] * 1001 for _ in range(n)]
+    ans = 0
+    for i in range(1, n):
+        for k in range(i):
+            j = nums[i] - nums[k] + 500
+            f[i][j] = max(f[i][j], f[k][j] + 1)
+            ans = max(ans, f[i][j])
+    return ans
+
+
+
