19530beff95aa4c0a777ca413f1d2461571499c8
JiangHai <hanayo@kayotin.cn>
2024-11-26 16:28:31 +0800

    update

--- shell脚本/01_安装jdk/00_安装jdk.sh
+++ shell脚本/01_安装jdk/00_安装jdk.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # 安装OpenJDK的脚本
+# wget -P /opt/ https://download.java.net/java/GA/jdk22.0.2/c9ecb94cd31b495da20a27d4581645e8/9/GPL/openjdk-22.0.2_linux-x64_bin.tar.gz
 live=1
 #判断自带JDK
 rpm -qa|grep java
@@ -29,8 +30,8 @@ function setJdk(){
     echo "配置环境变量"
     echo "#设置环境变量" >> /etc/profile
     echo "export JAVA_HOME=/opt/jdk/jdk-22.0.2" >> /etc/profile
-    echo "export CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/:$JAVA_HOME/jre/lib" >> /etc/profile
-    echo "export PATH=$JAVA_HOME/bin:$JAVA_HOME/jre/bin:$PATH:$HOMR" >> /etc/profile
+    echo "export CLASSPATH=\$CLASSPATH:\$JAVA_HOME/lib/:\$JAVA_HOME/jre/lib" >> /etc/profile
+    echo "export PATH=\$JAVA_HOME/bin:\$JAVA_HOME/jre/bin:\$PATH:\$HOMR" >> /etc/profile
   fi
 }
 setJdk
@@ -45,4 +46,4 @@ echo CLASSPATH=$CLASSPATH
 #查询java安装信息
 echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 echo 查询java安装信息
-java -version
+java --version
--- shell脚本/06_nginx相关/01_安装nginx.sh
+++ shell脚本/06_nginx相关/01_安装nginx.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+# 安装nginx
 
 yum install yum-utils -y
 rpm -Uvh http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm
@@ -11,5 +13,12 @@ systemctl enable nginx
 firewall-cmd --zone=public --add-port=80/tcp --permanent
 # reload防火墙
 firewall-cmd --reload
+# 查看当前开了哪些端口
+firewall-cmd --list-all
 # 启动nginx
 systemctl start nginx
+# 查看是否开机启动
+systemctl is-enabled nginx
+
+# nginx的reload命令
+nginx -s reload
